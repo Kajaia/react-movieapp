478568bd3c65c02ffb50286f16dd6b7891213d4b
Kajaia <lashakajaia31@gmail.com>
2025-05-17 20:30:29 +0400

    Added login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,51 @@
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+
 export default function LoginPage() {
-  return "Login";
+  const { login } = useContext(UserContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    login(email, password);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={(e) => handleForm(e)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+        <div>
+          <p>
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 }
